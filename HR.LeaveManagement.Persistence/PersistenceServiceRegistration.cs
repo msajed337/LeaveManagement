5d8f7bcf85fedb3c1850e22534d5ca61fd0f25c6
msajed337 <m.sajed337@gmail.com>
2023-12-03 00:27:50 +0330

    add DatabaseContext

---
+++ HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using HR.LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<HrDatabaseContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("HrDatabaseConnectionString"));
+            });
+
+            return services;
+        }
+    }
+}
