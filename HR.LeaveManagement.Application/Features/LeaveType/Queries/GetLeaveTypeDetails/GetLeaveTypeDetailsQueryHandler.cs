ea63c4044eea40d6060fefd7939d8ec14622857a
m.sajed <m.sajed@ebidar.net>
2023-12-02 20:04:00 +0330

    add Get query

--- HR.LeaveManagement.Application/Features/LeaveType/Queries/GetLeaveTypeDetails/GetLeaveTypeDetailsQueryHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetLeaveTypeDetails/GetLeaveTypeDetailsQueryHandler.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -23,6 +24,9 @@ namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveType
 		public async Task<LeaveTypeDetailsDto> Handle(GetLeaveTypeDetailsQuery request, CancellationToken cancellationToken)
 		{
 			var leaveType = await _leaveTypeRepository.GetByIdAsync(request.Id);
+
+			if (leaveType == null)
+				throw new NotFoundException(nameof(LeaveType), request.Id);
 
 			var data = _mapper.Map<LeaveTypeDetailsDto>(leaveType);
 
