35879da286686fa98ec78d93b662599001271b16
msajed337 <m.sajed337@gmail.com>
2023-12-04 23:18:01 +0330

    Implement LoggerAdapter

--- HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveTypeCommand/UpdateLeaveTypeCommandValidator.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveTypeCommand/UpdateLeaveTypeCommandValidator.cs
@@ -14,6 +14,10 @@ namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeave
 
         public UpdateLeaveTypeCommandValidator(ILeaveTypeRepository leaveTypeRepository)
         {
+            RuleFor(p => p.Id)
+                .NotNull()
+                .MustAsync(LeaveTypeMustExist);
+
             RuleFor(p => p.Name)
                 .NotEmpty().WithMessage("{PropertyName} is required")
                 .NotNull()
@@ -29,6 +33,12 @@ namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeave
 
 
             this._leaveTypeRepository = leaveTypeRepository;
+        }
+
+        private async Task<bool> LeaveTypeMustExist(int id, CancellationToken token)
+        {
+            var leaveType = await _leaveTypeRepository.GetByIdAsync(id);
+            return leaveType != null;
         }
 
         private async Task<bool> LeaveTypeNameUnique(UpdateLeaveTypeCommand command, CancellationToken token)
--- HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQueryHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQueryHandler.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Logging;
 using HR.LeaveManagement.Application.Contracts.Persistence;
 using MediatR;
 using System;
@@ -11,15 +12,18 @@ namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveT
 {
 	public class GetLeaveTypesQueryHandler : IRequestHandler<GetLeaveTypesQuery, List<LeaveTypeDto>>
 	{
+		private readonly IMapper _mapper;
+		private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IAppLogger<GetLeaveTypesQueryHandler> _logger;
 
-        public GetLeaveTypesQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        public GetLeaveTypesQueryHandler(IMapper mapper,
+			ILeaveTypeRepository leaveTypeRepository,
+			IAppLogger<GetLeaveTypesQueryHandler> logger)
         {
 			_mapper = mapper;
 			_leaveTypeRepository = leaveTypeRepository;
-		}
-
-		public IMapper _mapper { get; }
-		public ILeaveTypeRepository _leaveTypeRepository { get; }
+            _logger = logger;
+        }
 
 		public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypesQuery request, CancellationToken cancellationToken)
 		{
@@ -27,6 +31,7 @@ namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveT
 
 			var data = _mapper.Map<List<LeaveTypeDto>>(leaveTypes);
 
+			_logger.LogInformation("Leave types were retrieved successfully");
 			return data;
 		}
 	}
