96eed3748e723eb4bd2e2d0fbc2838e08562c6e9
m.sajed <m.sajed@ebidar.net>
2023-12-13 17:25:15 +0330

    Implement ExceptionMiddleware

--- HR.LeaveManagement.Application/Exceptions/BadRequestException.cs
+++ HR.LeaveManagement.Application/Exceptions/BadRequestException.cs
@@ -2,22 +2,18 @@
 
 namespace HR.LeaveManagement.Application.Exceptions
 {
-    public class BadRequestException : Exception
-    {
-        public BadRequestException(string message) : base(message)
-        {
+	public class BadRequestException : Exception
+	{
+		public BadRequestException(string message) : base(message)
+		{
 
-        }
+		}
 
-        public BadRequestException(string message, ValidationResult validationResult) : base(message)
-        {
-            ValidationErrors = new();
-            foreach (var error in validationResult.Errors)
-            {
-                ValidationErrors.Add(error.ErrorMessage);
-            }
-        }
+		public BadRequestException(string message, ValidationResult validationResult) : base(message)
+		{
+			ValidationErrors = validationResult.ToDictionary();
+		}
 
-        public List<string> ValidationErrors { get; set; }
-    }
+		public IDictionary<string, string[]> ValidationErrors { get; set; }
+	}
 }
