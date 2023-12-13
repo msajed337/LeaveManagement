efe408dd8f895687f42cfcb0382f2a331ff26003
m.sajed <m.sajed@ebidar.net>
2023-12-13 17:24:41 +0330

    Implement LeaveRequests

---
+++ HR.LeaveManagement.Api/Controllers/LeaveRequestsController.cs
@@ -0,0 +1,97 @@
+﻿using HR.LeaveManagement.Application.Features.LeaveRequest.Commands.CancelLeaveRequest;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Commands.ChangeLeaveRequestApproval;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Commands.CreateLeaveRequest;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Commands.DeleteLeaveRequest;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Commands.UpdateLeaveRequest;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveRequestDetail;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveRequestList;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR.LeaveManagement.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	[Authorize]
+	public class LeaveRequestsController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public LeaveRequestsController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<List<LeaveRequestListDto>>> Get(bool isLoggedInUser = false)
+		{
+			var leaveRequests = await _mediator.Send(new GetLeaveRequestListQuery());
+			return Ok(leaveRequests);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<LeaveRequestDetailsDto>> Get(int id)
+		{
+			var leaveRequest = await _mediator.Send(new GetLeaveRequestDetailQuery() { Id = id });
+			return Ok(leaveRequest);
+		}
+
+		[HttpPost]
+		[ProducesResponseType(201)]
+		[ProducesResponseType(400)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public async Task<ActionResult> Post(CreateLeaveRequestCommand leaveRequest)
+		{
+			var response = await _mediator.Send(leaveRequest);
+			return CreatedAtAction(nameof(Get), new { id = response });
+		}
+
+		[HttpPut]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(400)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesDefaultResponseType]
+		public async Task<ActionResult> Put(UpdateLeaveRequestCommand leaveRequest)
+		{
+			await _mediator.Send(leaveRequest);
+			return NoContent();
+		}
+
+		[HttpPut]
+		[Route("CancelRequest")]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(400)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesDefaultResponseType]
+		public async Task<ActionResult> CancelRequest(CancelLeaveRequestCommand leaveRequest)
+		{
+			await _mediator.Send(leaveRequest);
+			return NoContent();
+		}
+
+		[HttpPut]
+		[Route("UpdateApproval")]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(400)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesDefaultResponseType]
+		public async Task<ActionResult> UpdateApproval(ChangeLeaveRequestApprovalCommand leaveRequest)
+		{
+			await _mediator.Send(leaveRequest);
+			return NoContent();
+		}
+
+		[HttpDelete("{id}")]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesDefaultResponseType]
+		public async Task<ActionResult> Delete(int id)
+		{
+			var command = new DeleteLeaveRequestCommand() { Id = id };
+			await _mediator.Send(command);
+			return NoContent();
+		}
+	}
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Queries/GetLeaveRequestDetail/GetLeaveRequestDetailQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveRequestDetail
+{
+	public class GetLeaveRequestDetailQueryHandler : IRequestHandler<GetLeaveRequestDetailQuery, LeaveRequestDetailsDto>
+	{
+		private readonly ILeaveRequestRepository _leaveRequestRepository;
+		private readonly IMapper _mapper;
+
+		public GetLeaveRequestDetailQueryHandler(ILeaveRequestRepository leaveRequestRepository,
+			IMapper mapper)
+		{
+			_leaveRequestRepository = leaveRequestRepository;
+			_mapper = mapper;
+		}
+
+		public async Task<LeaveRequestDetailsDto> Handle(GetLeaveRequestDetailQuery request, CancellationToken cancellationToken)
+		{
+			var leaveRequest = _mapper.Map<LeaveRequestDetailsDto>(await
+				_leaveRequestRepository.GetLeaveRequestWithDetails(request.Id));
+
+			return leaveRequest;
+		}
+	}
+}
